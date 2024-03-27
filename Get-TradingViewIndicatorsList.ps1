3fa1b4ea4dea670c90e73a90b85bc1774d78527e
daynway <danywaystrige@gmail.com>
2024-03-27 21:52:13 +0100

    Improve array performances

--- Get-TradingViewIndicatorsList.ps1
+++ Get-TradingViewIndicatorsList.ps1
@@ -6,19 +6,20 @@
 .EXAMPLE
    Get-TradingViewIndicatorsList | Export-Csv -Path MostUseTradingViewIndicatorsList.csv -NoTypeInformation -Encoding utf8    
 #>
-function Get-TradingViewIndicatorsList
-{
+function Get-TradingViewIndicatorsList {
     [CmdletBinding()]
     $allLeters = 'a'..'z'
-    $indicatorsList = @()
+    $indicatorsList = New-Object System.Collections.ArrayList
 
     foreach ($letter in $allLeters) {
         $tradingViewIndicatorsUrl = "https://www.tradingview.com/pubscripts-suggest-json/?search=$letter"
-        $indicatorsList += Invoke-RestMethod -Uri $tradingViewIndicatorsUrl
+        $result = Invoke-RestMethod -Uri $tradingViewIndicatorsUrl
+        $null = $indicatorsList.add($result.results)
     }
 
     # get all indicators with agreeCount > 10000, display only scriptName, agreeCount, and author
-    $indicatorsList.results | Where-Object { $_.agreeCount -gt 10000} | 
-        Select-Object -Property scriptName, agreeCount, @{Name='author'; Expression={$_.author.username}} -Unique | 
-        Sort-Object -Property agreeCount -Descending
+    $indicatorsList | ForEach-Object { $_ } |
+      Where-Object { $_.agreeCount -gt 10000} |
+      Select-Object -Property scriptName, agreeCount, @{Name='author'; Expression={$_.author.username}}, @{Name='type'; Expression={$_.extra.kind}} -Unique | 
+      Sort-Object -Property agreeCount -Descending
 }
